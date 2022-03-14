23fa0ff891ae792470f4b6e26833d2cdce6f270f
Paul Danelli <prdanelli@gmail.com>
2022-03-14 16:23:26 +0000

    Catch connection timeout error and retry

--- app/jobs/hyrax/hirmeos/hirmeos_work_updater_job.rb
+++ app/jobs/hyrax/hirmeos/hirmeos_work_updater_job.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
+
 module Hyrax
   module Hirmeos
     class HirmeosWorkUpdaterJob < ApplicationJob
+      retry_on Faraday::TimeoutError, wait: 60.seconds
+
       def perform(resource_id)
         resource = ActiveFedora::Base.find(resource_id)
         service.submit_diff_to_hirmeos(resource)
